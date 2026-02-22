023be063460b87786fc508a4713743688e5bc77b
Wouter van Wijk <woutervanwijk@gmail.com>
2026-02-22 13:03:32 +0100

    add macos to buildapps (commented out for now)

--- buildapps.sh
+++ buildapps.sh
@@ -7,3 +7,7 @@ mv build/ios/archive/Runner.xcarchive "build/ViewSourceVibe.xcarchive"
 flutter build appbundle --release
 rm -f "build/ViewSourceVibe.aab"
 mv build/app/outputs/bundle/release/app-release.aab "build/ViewSourceVibe.aab"
+
+# flutter build macos --release
+# rm -drf  "build/View Source Vibe.app"
+# mv "build/macos/Build/Products/Release/View Source Vibe.app" "build/View Source Vibe.app"
